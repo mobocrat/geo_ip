36b7763a3381c931b39b4e3f19af0cc7ff84f3bb
javad ghaedi <ghaedi1993@gmail.com>
2021-11-12 20:41:24 +0330

    remove ridundant comments

--- main.go
+++ main.go
@@ -6,47 +6,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// type LocationResponse struct {
-// 	Status      string  `json:"status"`
-// 	Country     string  `json:"country"`
-// 	CountryCode string  `json:"countryCode"`
-// 	Region      string  `json:"region"`
-// 	RegionName  string  `json:"regionName"`
-// 	City        string  `json:"city"`
-// 	Zip         string  `json:"zip"`
-// 	Lat         float64 `json:"lat"`
-// 	Lon         float64 `json:"lon"`
-// 	Timezone    string  `json:"timezone"`
-// 	Isp         string  `json:"isp"`
-// 	Org         string  `json:"org"`
-// 	As          string  `json:"as"`
-// 	Query       string  `json:"query"`
-// }
-
-//create a echo middleware
-// func ipLocater(next echo.HandlerFunc) echo.HandlerFunc {
-// 	return func(c echo.Context) error {
-// 		requestIp := c.RealIP()
-// 		res, err := http.Get("http://ip-api.com/json/" + requestIp)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 		}
-// 		respBody, err := ioutil.ReadAll(res.Body)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			locationStatsMutex.Lock()
-// 		}
-// 		var loc LocationResponse
-// 		json.Unmarshal(respBody, &loc)
-// 		fmt.Println(loc.Country)
-// 		res.Body.Close()
-// 		incrementCountryNum(&loc)
-// 		return next(c)
-// 	}
-// }
-
-// send a get request
-
 func main() {
 	go incrementer()
 	e := echo.New()
